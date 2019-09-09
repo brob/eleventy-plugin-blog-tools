d09f5c3789ad7b44ccf75ec813aaa569789176eb
Bryan Robinson <bryanlrobinson@gmail.com>
2019-09-09 13:25:37 -0500

    Adds blockquote file

---
+++ src/blockquote.js
@@ -0,0 +1,19 @@
+const markdownIt = require("markdown-it");
+let md = new markdownIt();
+
+
+
+
+module.exports = function(content, citation, className="") {
+
+    let addedClass = className ? ` class="${className}"` : "";
+
+    
+    content = md.renderInline(content);
+    
+    return `<blockquote${addedClass}>
+        ${content}
+        <cite>${citation}</cite>
+    </blockquote>
+    `
+}
