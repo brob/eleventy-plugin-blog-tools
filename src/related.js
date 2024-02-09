98336bf87568770a8c5ffe6a3a6ae8b29f2c1ca0
BigBlueHat <byoung@bigbluehat.com>
2024-02-09 13:56:29 -0500

    Fix related when missing categorizeBy value

--- src/related.js
+++ src/related.js
@@ -1,16 +1,18 @@
-module.exports = function(items, categorizeBy, categoryArray, threshold = 1, excludedSlug = "") {
-    let posts = Array.from(items).filter(item => {
-      return item.fileSlug != excludedSlug // removes "excluded item" aka the current item
-    });
+module.exports = function(items, categorizeBy, categoryArray = [], threshold = 1, excludedSlug = "") {
+  let posts = Array.from(items).filter(item => {
+    return item.fileSlug != excludedSlug // removes "excluded item" aka the current item
+  });
 
-    let filteredPosts = posts.filter(item => {
-      let matchedCount = 0; // Counter to match our threshold
+  let filteredPosts = posts.filter(item => {
+    let matchedCount = 0; // Counter to match our threshold
+    if (categorizeBy in item.data) {
       item.data[categorizeBy].forEach(cat => {
         if (categoryArray.includes(cat)) {
           return matchedCount++
         }
       });
-      return (matchedCount >= threshold);
-    })
-    return filteredPosts
+    }
+    return (matchedCount >= threshold);
+  })
+  return filteredPosts
 }
